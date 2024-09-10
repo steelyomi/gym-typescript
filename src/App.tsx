1defcc6b767e80dbc02fb14506460d223df64ac6
steelyomi <bmlasisi@gmail.com>
2024-09-10 14:23:41 +0100

    classes page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
+import OurClasses from "@/scenes/ourClasses";
 import Benefits from "@/scenes/benefits";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
@@ -31,6 +32,7 @@ const App = () => {
       />
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
     </div>
   );
 };
